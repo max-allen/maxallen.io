708c10fee4c0e20965ca06a96ebfb357e8d10f61
Max Allen <maxallen@transfix.io>
2019-02-23 15:28:17 -0500

    About functional

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import 'typeface-montserrat'
 import 'typeface-merriweather'
@@ -7,24 +7,15 @@ import headshot from './mallen_headshot_7_29_18.png'
 
 import { rhythm } from '../utils/typography'
 
+const About = () => (
+  <div style={{ textAlign: 'left' }}>
 
-class About extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          textAlign: 'left',
-        }}
-      >
-
-        <p>I'm a Software Engineer based in <strong>New York City. </strong>
-        At the moment, I'm focused on building UI's for the web.
-        My interests run the gamut, but right now I'm mostly coding
-        React JS for FinTech companies.
-        </p>
-      </div>
-    )
-  }
-}
+    <p>I'm a Software Engineer based in <strong>New York City. </strong>
+    At the moment, I'm focused on building UI's for the web.
+    My interests run the gamut, but right now I'm mostly coding
+    React JS for FinTech companies.
+    </p>
+  </div>
+)
 
 export default About
