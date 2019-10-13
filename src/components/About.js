923735e1a4536a8b9534407c9dbdf0b4fcbb840c
max-allen <contact@maxallen.io>
2019-10-13 15:28:51 -0400

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,10 @@ const About = () => (
   <div style={{ textAlign: 'left' }}>
 
     <p>I'm a Software Engineer based in <strong>New York City. </strong>
-    At the moment, I'm focused on building UI's for the web.
+    At the moment, I'm building UI's for both the web and mobile.
     My interests run the gamut, but right now I'm mostly coding
-    React JS for FinTech companies.
+    React JS for a freight brokerage named <a target='_blank' href='https://transfix.io/'>Transfix</a>,
+    one of the companies focused on <a target='_blank' href='https://www.supplychaindive.com/news/digital-freight-brokerage/549692/'>taking freight digital.</a>
     </p>
   </div>
 )
