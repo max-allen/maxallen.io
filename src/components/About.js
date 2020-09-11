8b1254ec6f8f5928127e4eacac3d4ea9c5a49d09
max-allen <contact@maxallen.io>
2020-09-11 06:13:29 -0400

    deletes md text

--- src/components/About.js
+++ src/components/About.js
@@ -17,16 +17,16 @@ const About = () => (
       lineHeight: rhythm(1.2)
     }}
   >
-      <p>I'm a Software Engineer based in { isMobile && <br /> } <strong>New York City.</strong>
-      &nbsp;Currently building things at <a href='https://www.nytimes.com/'>The New York Times,</a>
-      &nbsp;and previously an engineer at <a href='https://transfix.io/'>Transfix.</a>
-      </p>
+    <p>I'm a Software Engineer based in { isMobile && <br /> } <strong>New York City.</strong>
+    &nbsp;Currently building things at <a href='https://www.nytimes.com/'>The New York Times,</a>
+    &nbsp;and previously an engineer at <a href='https://transfix.io/'>Transfix.</a>
+    </p>
 
-      <p>I've consulted for a number of startups based in NYC, helping them build both web and
-      mobile applications. The depth of my experience is the greatest client-side, but I consider myself
-      a generalist and am comfortable across the stack. I've benefited from being both conscientious and industrious,
-      but my curiosity and enthusiasm at the intersection of product & engineering are my greatest assets as an engineer.
-      </p>
+    <p>I've consulted for a number of startups based in NYC, helping them build both web and
+    mobile applications. The depth of my experience is the greatest client-side, but I consider myself
+    a generalist and am comfortable across the stack. I've benefited from being both conscientious and industrious,
+    but my curiosity and enthusiasm at the intersection of product & engineering are my greatest assets as an engineer.
+    </p>
   </LineHeightOverride>
 )
 
--- src/pages/new-page/index.md
+++ src/pages/new-page/index.md
@@ -1,23 +1,6 @@
 ---
 title: Untitled
-date: "2018-09-27"
+date: "2020-09-11"
 ---
 
-Occasionally I've encountered questions like, *"How can I start learning React,"* *"What are React's prerequisites,"*
-or *"What should I know in advance so I can learn React efficiently?"* This article aims to answer these questions. It
-assumes basic knowledge of JavaScript and web development.
-
-### The Basics
-
-React  
-
-
-```javascript
-const functionalComponent = () => {
-	return (
-		<div>
-			{this.state.user}
-		</div>
-	)
-}
-```
+A temporary markdown file.
